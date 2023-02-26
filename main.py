862d8f5982132f2dd7badc0497a68ea37d206daa
profmohamed <mohamedsalem.ca@gmail.com>
2023-02-26 21:56:55 +0200

    create paddle class

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 # This is a pong game
+from turtle import Screen, Turtle
+from paddle import Paddle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.listen()
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+top_paddle = Paddle((0, 300))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+screen.exitonclick()
