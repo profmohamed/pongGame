d4f7570fbe8c2b64f73f8e7fd259819a39d19d9e
profmohamed <mohamedsalem.ca@gmail.com>
2023-02-27 16:43:11 +0200

    create paddle class

--- paddle.py
+++ paddle.py
@@ -13,9 +13,11 @@ class Paddle(Turtle):
         self.penup()
         self.goto(position)
 
+
 def go_up(self):
     new_y = self.ycor() + 20
     self.goto(self.xcor(), new_y)
+
 
 
 def go_down(self):
