b0bac8d31fca9e541982c7230a952cd4406bb584
profmohamed <mohamedsalem.ca@gmail.com>
2023-02-27 21:29:37 +0200

    create ball class

--- main.py
+++ main.py
@@ -28,5 +28,8 @@ while game_is_on:
     screen.update()
     ball.move()
 
+#     # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
 screen.exitonclick()
